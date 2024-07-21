f864e91b9bd3c3f790ed00ec8c7ecc18e7cfc1b5
Anuj Kumar <anuj9654949185@gmail.com>
2024-07-21 18:42:34 +0530

    routes, contoller setup

--- src/app.js
+++ src/app.js
@@ -14,5 +14,17 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+// url will look like
+// https://localhost:3000/api/v1/users/register
+
 
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,23 +1,23 @@
 // wrapper to handle talk with database
 // using async await
 
-const asyncHandler = (func)= async (req, res, next) => {
-    try {
-        await func(req, res, next)
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })   
-    }
-}
-
-// const asyncHandler = (requestHandler) => {
-//     (req, res, next) => {
-//         Promise.resolve(requestHandler(req, res, next)).
-//         catch((err) => next(err))
+// const asyncHandler = (func) = async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })   
 //     }
-
 // }
 
-// export {asyncHandler}
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+
+}
+
+export {asyncHandler}
